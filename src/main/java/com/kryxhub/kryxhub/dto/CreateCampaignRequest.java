a7e7c10d5c8e98535f69cb8b77de15d86b71b13c
SFxMeeT <meetvaghasiya086@gmail.com>
2026-04-30 05:53:53 +0530

    Fixed CreateCampaignRequest DTO

--- src/main/java/com/kryxhub/kryxhub/dto/CreateCampaignRequest.java
+++ src/main/java/com/kryxhub/kryxhub/dto/CreateCampaignRequest.java
@@ -9,11 +9,6 @@ import com.kryxhub.kryxhub.enums.Platforms;
 import com.kryxhub.kryxhub.enums.LinkLabel;
 import com.kryxhub.kryxhub.enums.QuestionType;
 
-package com.kryxhub.kryxhub.dto;
-
-import java.math.BigDecimal;
-import java.util.List;
-
 public class CreateCampaignRequest {
 
     private String title;
@@ -36,109 +31,46 @@ public class CreateCampaignRequest {
     public CreateCampaignRequest() {
     }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
-    public String getDescription() {
-        return description;
-    }
+    public CampaignType getType() { return type; }
+    public void setType(CampaignType type) { this.type = type; }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public CampaignCategory getCategory() { return category; }
+    public void setCategory(CampaignCategory category) { this.category = category; }
 
-    public CampaignType getType() {
-        return type;
-    }
+    public String getThumbnailUrl() { return thumbnailUrl; }
+    public void setThumbnailUrl(String thumbnailUrl) { this.thumbnailUrl = thumbnailUrl; }
 
-    public void setType(CampaignType type) {
-        this.type = type;
-    }
+    public BigDecimal getTotalBudget() { return totalBudget; }
+    public void setTotalBudget(BigDecimal totalBudget) { this.totalBudget = totalBudget; }
 
-    public CampaignCategory getCategory() {
-        return category;
-    }
+    public List<PlatformDto> getPlatforms() { return platforms; }
+    public void setPlatforms(List<PlatformDto> platforms) { this.platforms = platforms; }
 
-    public void setCategory(CampaignCategory category) {
-        this.category = category;
-    }
+    public List<String> getRules() { return rules; }
+    public void setRules(List<String> rules) { this.rules = rules; }
 
-    public String getThumbnailUrl() {
-        return thumbnailUrl;
-    }
+    public List<LinkDto> getLinks() { return links; }
+    public void setLinks(List<LinkDto> links) { this.links = links; }
 
-    public void setThumbnailUrl(String thumbnailUrl) {
-        this.thumbnailUrl = thumbnailUrl;
-    }
+    public List<FaqDto> getFaqs() { return faqs; }
+    public void setFaqs(List<FaqDto> faqs) { this.faqs = faqs; }
 
-    public BigDecimal getTotalBudget() {
-        return totalBudget;
-    }
+    public Boolean getRequiresApplication() { return requiresApplication; }
+    public void setRequiresApplication(Boolean requiresApplication) { this.requiresApplication = requiresApplication; }
 
-    public void setTotalBudget(BigDecimal totalBudget) {
-        this.totalBudget = totalBudget;
-    }
+    public Boolean getShowOnDiscover() { return showOnDiscover; }
+    public void setShowOnDiscover(Boolean showOnDiscover) { this.showOnDiscover = showOnDiscover; }
 
-    public List<PlatformDto> getPlatforms() {
-        return platforms;
-    }
+    public List<QuestionDto> getQuestions() { return questions; }
+    public void setQuestions(List<QuestionDto> questions) { this.questions = questions; }
 
-    public void setPlatforms(List<PlatformDto> platforms) {
-        this.platforms = platforms;
-    }
-
-    public List<String> getRules() {
-        return rules;
-    }
-
-    public void setRules(List<String> rules) {
-        this.rules = rules;
-    }
-
-    public List<LinkDto> getLinks() {
-        return links;
-    }
-
-    public void setLinks(List<LinkDto> links) {
-        this.links = links;
-    }
-
-    public List<FaqDto> getFaqs() {
-        return faqs;
-    }
-
-    public void setFaqs(List<FaqDto> faqs) {
-        this.faqs = faqs;
-    }
-
-    public Boolean getRequiresApplication() {
-        return requiresApplication;
-    }
-
-    public void setRequiresApplication(Boolean requiresApplication) {
-        this.requiresApplication = requiresApplication;
-    }
-
-    public Boolean getShowOnDiscover() {
-        return showOnDiscover;
-    }
-
-    public void setShowOnDiscover(Boolean showOnDiscover) {
-        this.showOnDiscover = showOnDiscover;
-    }
-
-    public List<QuestionDto> getQuestions() {
-        return questions;
-    }
-
-    public void setQuestions(List<QuestionDto> questions) {
-        this.questions = questions;
-    }
+    // --- Nested Classes ---
 
     public static class PlatformDto {
         private Platforms platformName;
@@ -146,88 +78,45 @@ public class CreateCampaignRequest {
         private BigDecimal minPayout;
         private BigDecimal maxPayout;
 
-        public PlatformDto() {
-        }
+        public PlatformDto() {}
 
-        public Platforms getPlatformName() {
-            return platformName;
-        }
+        public Platforms getPlatformName() { return platformName; }
+        public void setPlatformName(Platforms platformName) { this.platformName = platformName; }
 
-        public void setPlatformName(Platforms platformName) {
-            this.platformName = platformName;
-        }
+        public BigDecimal getCpmRate() { return cpmRate; }
+        public void setCpmRate(BigDecimal cpmRate) { this.cpmRate = cpmRate; }
 
-        public BigDecimal getCpmRate() {
-            return cpmRate;
-        }
+        public BigDecimal getMinPayout() { return minPayout; }
+        public void setMinPayout(BigDecimal minPayout) { this.minPayout = minPayout; }
 
-        public void setCpmRate(BigDecimal cpmRate) {
-            this.cpmRate = cpmRate;
-        }
-
-        public BigDecimal getMinPayout() {
-            return minPayout;
-        }
-
-        public void setMinPayout(BigDecimal minPayout) {
-            this.minPayout = minPayout;
-        }
-
-        public BigDecimal getMaxPayout() {
-            return maxPayout;
-        }
-
-        public void setMaxPayout(BigDecimal maxPayout) {
-            this.maxPayout = maxPayout;
-        }
+        public BigDecimal getMaxPayout() { return maxPayout; }
+        public void setMaxPayout(BigDecimal maxPayout) { this.maxPayout = maxPayout; }
     }
 
     public static class LinkDto {
         private LinkLabel label;
         private String url;
 
-        public LinkDto() {
-        }
+        public LinkDto() {}
 
-        public LinkLabel getLabel() {
-            return label;
-        }
+        public LinkLabel getLabel() { return label; }
+        public void setLabel(LinkLabel label) { this.label = label; }
 
-        public void setLabel(LinkLabel label) {
-            this.label = label;
-        }
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
+        public String getUrl() { return url; }
+        public void setUrl(String url) { this.url = url; }
     }
 
     public static class FaqDto {
         private String question;
         private String answer;
 
-        public FaqDto() {
-        }
+        public FaqDto() {}
 
-        public String getQuestion() {
-            return question;
-        }
+        public String getQuestion() { return question; }
+        public void setQuestion(String question) { this.question = question; }
 
-        public void setQuestion(String question) {
-            this.question = question;
-        }
-
-        public String getAnswer() {
-            return answer;
-        }
-
-        public void setAnswer(String answer) {
-            this.answer = answer;
-        }
+        public String getAnswer() { return answer; }
+        public void setAnswer(String answer) { this.answer = answer; }
     }
 
     public static class QuestionDto {
@@ -235,31 +124,15 @@ public class CreateCampaignRequest {
         private QuestionType questionType;
         private Boolean isRequired;
 
-        public QuestionDto() {
-        }
+        public QuestionDto() {}
 
-        public String getQuestionText() {
-            return questionText;
-        }
+        public String getQuestionText() { return questionText; }
+        public void setQuestionText(String questionText) { this.questionText = questionText; }
 
-        public void setQuestionText(String questionText) {
-            this.questionText = questionText;
-        }
+        public QuestionType getQuestionType() { return questionType; }
+        public void setQuestionType(QuestionType questionType) { this.questionType = questionType; }
 
-        public QuestionType getQuestionType() {
-            return questionType;
-        }
-
-        public void setQuestionType(QuestionType questionType) {
-            this.questionType = questionType;
-        }
-
-        public Boolean getIsRequired() {
-            return isRequired;
-        }
-
-        public void setIsRequired(Boolean isRequired) {
-            this.isRequired = isRequired;
-        }
+        public Boolean getIsRequired() { return isRequired; }
+        public void setIsRequired(Boolean isRequired) { this.isRequired = isRequired; }
     }
 }
