92732150bd78612080fef6942cd60b9e6f32ab9f
SFxMeeT <meetvaghasiya086@gmail.com>
2026-04-30 10:02:15 +0530

    Again fixed import issue

--- src/main/java/com/kryxhub/kryxhub/KryxhubApplication.java
+++ src/main/java/com/kryxhub/kryxhub/KryxhubApplication.java
@@ -3,6 +3,7 @@ package com.kryxhub.kryxhub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableTransactionManagement
