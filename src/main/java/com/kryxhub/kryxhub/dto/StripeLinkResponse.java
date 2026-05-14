16f7763e59423375866a92571c033272664343cf
SFxMeeT <meetvaghasiya086@gmail.com>
2026-05-14 19:02:42 +0530

    Stripe onboard added

---
+++ src/main/java/com/kryxhub/kryxhub/dto/StripeLinkResponse.java
@@ -0,0 +1,17 @@
+package com.kryxhub.kryxhub.dto;
+
+public class StripeLinkResponse {
+    private String url;
+
+    public StripeLinkResponse(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
